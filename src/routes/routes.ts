1da172816eb73be214beb2d10e69aa37e5694911
IvanMYT-SDP <174562939+IvanMYT-SDP@users.noreply.github.com>
2025-11-16 09:00:50 -0600

    Add files via upload

---
+++ src/routes/routes.ts
@@ -0,0 +1,8 @@
+export const HOME = '/';
+export const LOGIN = '/login';
+export const REGISTER = '/register';
+export const WATCH = '/watch';
+export const MARKETPLACE = '/marketplace';
+export const GROUP = '/group';
+export const GAMING = '/gaming';
+export const PROFILE = '/profile';
